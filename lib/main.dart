c7416642c797324f5af32c9dbd838ccec0492503
kevin <kevinbpatoliya23@gmail.com>
2023-02-12 19:00:08 +0530

    ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -33,35 +33,96 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body: Center(
-            child: InkWell(
-          onTap: () {
-            print("ontab");
-          },
-          onDoubleTap: () {
-            print("onDoubleTab");
-          },
-          onLongPress: () {
-            print("onLongTab");
-          },
-          child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.amber,
-              child: Center(
-                child: InkWell(
-                  onTap: () {
-                    print("Text Widget Tapped");
-                  },
-                  child: Text(
-                    "Click here",
-                    style: TextStyle(fontSize: 23, fontWeight: FontWeight.w800),
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 11),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 11),
+                        height: 200,
+                        width: 200,
+                        color: Colors.lightGreen,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 11),
+                        height: 200,
+                        width: 200,
+                        color: Colors.green,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 11),
+                        height: 200,
+                        width: 200,
+                        color: Colors.pink,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 11),
+                        height: 200,
+                        width: 200,
+                        color: Colors.purple,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 11),
+                        height: 200,
+                        width: 200,
+                        color: Colors.yellow,
+                      ),
+                    ],
                   ),
                 ),
-              )),
-        )));
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                color: Colors.lightBlue,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                color: Colors.black26,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                width: 200,
+                color: Colors.lightGreen,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                color: Colors.black26,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                width: 200,
+                color: Colors.lightGreen,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                width: 200,
+                color: Colors.black26,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 11),
+                height: 200,
+                color: Colors.lightGreen,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
